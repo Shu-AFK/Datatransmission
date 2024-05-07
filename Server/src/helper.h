10232e71ec7fad98811ca4ba3989b0db875b86d1
Khvostov Aleksey <hvostov.a.u@gmail.com>
2024-05-07 15:22:06 +0300

    Added recursive path search. Changed sqlite3.dll into sqlite3.lib in CMakeLists

--- Server/src/helper.h
+++ Server/src/helper.h
@@ -2,8 +2,12 @@
 #define DATATRANSMISSION_HELPER_H
 
 #include <string>
+#include <optional>
+#include <filesystem>
+#include <iostream>
 
 int run_init(std::basic_string<char> port);
 int create_start_script(const std::string& cwd, const std::string& port);
+std::optional<std::filesystem::path> find_path(std::filesystem::path start_path, std::string target);
 
 #endif //DATATRANSMISSION_HELPER_H
